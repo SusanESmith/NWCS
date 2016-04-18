6b50b78fb3dbcf4743162811395ffd64e6d0b523
Susan Smith <susan.smith1024@gmail.com>
2016-04-18 11:38:07 -0500

    DB

--- nwcsdatabase.php
+++ nwcsdatabase.php
@@ -5,12 +5,12 @@
 //db connection
 	$dsn='mysql:host=45.55.134.169; dbname=NWCS';
 	$username='root';
-	//$password='yEKmgRWvpP';
+	$password='yEKmgRWvpP';
 
 
 //PDO object
 	try{
-		$db=new PDO($dsn, $username);// $password);
+		$db=new PDO($dsn, $username, $password);
 			//echo "Success";
 	}catch(PDOException $e){
 		$error=$e->getMessage();
